eb2c0c238bb5ae84d9570f112bdacb13b3ecb4f3
Yury Blagoveshchenskiy <y.blagoveshchenskiy@rambler-co.ru>
2018-12-24 14:53:39 +0300

    CLI

---
+++ src/cli.py
@@ -0,0 +1,50 @@
+import os
+
+from src.openapi_generator import OpenAPIGenerator
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Turn files with pydantic models to OpenAPI JSON schema. '
+                                                 'Config file have priority over arguments.')
+    parser.add_argument('--config', type=str)
+    parser.add_argument('--modules', nargs='+', help='files with Pydantic models',
+                        type=str, metavar='module')
+    parser.add_argument('--title', type=str, help='schema title')
+    parser.add_argument('--indent', type=int, help='output indent', default=2)
+    parser.add_argument('--prefix', type=str, help='reference prefix', default='#/components/schemas/')
+    parser.add_argument('--file', type=argparse.FileType, help='description file')
+    parser.add_argument('--text', type=str, help='description text')
+    parser.add_argument('--output', type=str, help='output file')
+
+    args = parser.parse_args()
+    os.chdir('..')
+
+    if args.config:
+        from configparser import ConfigParser
+        config_parser = ConfigParser(allow_no_value=True)
+        config_parser.read(args.config)
+
+        generator_options = config_parser['generator options']
+        title = generator_options.get('title') or args.title
+        indent = int(generator_options.get('indent') or args.indent)
+        prefix = generator_options.get('prefix').strip('"') or args.prefix
+        description = generator_options.get('description') or args.description
+        modules = [k for k in config_parser.get('modules')] or args.modules
+    else:
+        title = args.title
+        indent = args.indent
+        prefix = args.prefix
+        modules = args.modules
+        description = args.description
+    generator = OpenAPIGenerator(title=title, indent=indent, ref_prefix=prefix,
+                                 description=description)
+
+    for module in modules:
+        generator.load_module(module)
+
+    if args.output:
+        with open(args.output, 'w') as o:
+            o.write(generator.render())
+    else:
+        print(generator.render())
--- src/openapi_generator.py
+++ src/openapi_generator.py
@@ -1,68 +1,48 @@
-import textwrap
+import json
 from importlib.util import spec_from_file_location, module_from_spec
-from typing import Dict, Union
+from typing import Optional
 
 from pydantic import BaseModel
-from pydantic.fields import Field
+from pydantic.schema import schema
 
 
 class OpenAPIGenerator:
-    TYPE_MAPPING = {
-        'int': 'integer',
-        'float': 'number',
-        'str': 'string',
-        'bool': 'boolean',
-        'list': 'array',
-        'object': 'object'
-    }
 
-    def __init__(self):
-        self.ref_cache: Dict[str, str] = {}
-        self.specs = {}
+    def __init__(self, *,
+                 indent: int=2,
+                 ref_prefix: str = '#/components/schemas/',
+                 title: Optional[str] = None,
+                 by_alias: bool = True,
+                 description: Optional[str] = None
+                 ):
+        self.description = description
+        self.by_alias = by_alias
+        self.indent = indent
+        self.ref_prefix = ref_prefix
+        self.title = title
+        self.models = []
 
-    def load_file(self, module_path: str):
+    @property
+    def schema(self) -> dict:
+        return schema(
+            self.models,
+            by_alias=self.by_alias,
+            title=self.title,
+            description=self.description,
+            ref_prefix=self.ref_prefix
+        )
+
+    def render(self) -> str:
+        return json.dumps(self.schema, indent=self.indent)
+
+    def load_module(self, module_path: str):
         module_name = module_path.rstrip('.py').replace('/', '.')
         spec = spec_from_file_location(module_name, module_path)
         module = module_from_spec(spec)
         spec.loader.exec_module(module)
         for model_name in module.__all__:
-            model = getattr(module, model_name)
-            swagger = self.model_to_swagger(model, model_name)
-            self.specs[model_name] = swagger
+            self.models.append(getattr(module, model_name))
 
-    TYPE = 'type: {type}'
-    TITLE = 'title: {title}'
-    REF = '{name}:' \
-          '  $ref: {ref}'
-
-    def model_to_swagger(self, model: Union[BaseModel, Field], model_name: str, indent: int=2) -> str:
-        schema = model.schema()
-        required = []
-        properties = []
-        ref = self.ref_cache.get(model_name)
-        # enable recursive calls
-        if schema['type'] == 'object':
-            for key, prop in schema['properties'].items():
-                if prop.get('required'):
-                    required.append(key)
-                prop_obj = model.__fields__[key]
-                prop_swagger = self.model_to_swagger(prop_obj, key, indent)
-                prop_swagger = textwrap.indent(prop_swagger, ' ' * indent)
-                properties.append(prop_swagger)
-
-        lines = [
-            model_name + ':',
-            self.TITLE.format(title=schema['title']),
-            self.TYPE.format(type=self.TYPE_MAPPING[schema['type']]),
-        ]
-
-        if properties:
-            properties = '\n'.join(properties)
-            lines.append(properties)
-        if required:
-            required = '\n'.join(['  - ' + item for item in required])
-            lines.append(required)
-
-        return '\n'.join(lines)
-
-
+    @staticmethod
+    def model_to_swagger(model: BaseModel, indent: int = 2):
+        return json.dumps(model.schema(), indent=indent)
